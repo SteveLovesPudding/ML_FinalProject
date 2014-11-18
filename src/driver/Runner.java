36c03df178644a830ef67c5862b195e05fb66cec
Steven <stevenlin1992@gmail.com>
2014-11-17 20:39:55 -0500

    Begin implementation for checking for intersection

--- src/driver/Runner.java
+++ src/driver/Runner.java
@@ -5,7 +5,7 @@ import robotArm.Arm;
 public class Runner {
 
 	public static void main(String[] args){
-		double[] armLengths = {2.0, 1.5, 3.0};
+		double[] armLengths = {2.0, 1.3, 2.1};
 		double[] angles = {Math.PI/4, Math.PI/2, Math.PI/2, Math.PI/2, Math.PI/4, Math.PI/2, 1.0, 3.0, 1.0};
 		
 		Arm arm = new Arm(armLengths);
--- src/robotArm/Arm.java
+++ src/robotArm/Arm.java
@@ -1,5 +1,8 @@
 package robotArm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Arm {
 
 	private double[] armLengths;
@@ -20,36 +23,40 @@ public class Arm {
 
 		double xend = 0, yend = 0, zend = 0;
 
+		// check that enough dimensions are sent
 		if (angles.length == armLengths.length * 3) {
-			// 3 dimensions
+
+			Map<Integer, double[]> armEnds = new HashMap<Integer, double[]>();
 			double x = 1, y = 1, z = 1;
 			for (int i = 0; i < armLengths.length; i++) {
 
-				double xangle = angles[0 + 3 * i], yangle = angles[1 + 3 * i], zangle = angles[2 + 3 * i];
+				// {start x, start y, start z, end x, end y, end z}
+				double[] ends = new double[6];
+				ends[0] = xend;
+				ends[1] = yend;
+				ends[2] = zend;
+
+				int adjust = 3 * i;
+				double[] vector = findOrientation(new double[] { x, y, z },
+						new double[] { angles[0 + adjust], angles[1 + adjust],
+								angles[2 + adjust] });
+				vector = convertToUnitVector(new double[] { x, y, z });
+				x = vector[0];
+				y = vector[1];
+				z = vector[2];
+
 				double armLength = armLengths[i];
 
-				// rotation along x
-				y *= Math.cos(xangle);
-				z *= Math.sin(xangle);
-				// rotation along y
-				x *= Math.cos(yangle);
-				z *= Math.sin(yangle);
-				// rotation along z
-				y *= Math.sin(zangle);
-				x *= Math.cos(zangle);
+				ends[3] = (xend += x * armLength);
+				ends[4] = (yend += y * armLength);
+				ends[5] = (zend += z * armLength);
 
-				// adjust to unit vector
-				double vectorLength = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)
-						+ Math.pow(z, 2));
-				x /= vectorLength;
-				y /= vectorLength;
-				z /= vectorLength;
+				armEnds.put(i, ends);
 
-				xend += x * armLength;
-				yend += y * armLength;
-				zend += z * armLength;
-
-				//TODO Check for cross overs
+				System.out.println(Math.sqrt(Math.pow(x * armLength, 2)
+						+ Math.pow(y * armLength, 2)
+						+ Math.pow(z * armLength, 2)));
+				// TODO Check for cross overs
 			}
 		} else {
 			System.err.println("Wrong number of angular inputs");
@@ -58,5 +65,56 @@ public class Arm {
 		System.out.println(xend + " " + yend + " " + zend);
 
 		return new double[] { xend, yend, zend };
+	}
+
+	/**
+	 * 
+	 * @param vector
+	 *            {x,y,z}
+	 * @return
+	 */
+	private double[] convertToUnitVector(double[] vector) {
+
+		double vectorLength = Math.sqrt(Math.pow(vector[0], 2)
+				+ Math.pow(vector[1], 2) + Math.pow(vector[2], 2));
+
+		return new double[] { vector[0] / vectorLength,
+				vector[1] / vectorLength, vector[2] / vectorLength };
+	}
+
+	/**
+	 * 
+	 * @param original
+	 *            Original orientation {x,y,z}
+	 * @param angles
+	 *            New orientation angle {x_angle, y_angle, z_angle}
+	 * @return
+	 */
+	private double[] findOrientation(double[] original, double[] angles) {
+		double x = original[0], y = original[1], z = original[2];
+		double xangle = angles[0], yangle = angles[1], zangle = angles[2];
+
+		y *= Math.cos(xangle);
+		z *= Math.sin(xangle);
+		// rotation along y
+		x *= Math.cos(yangle);
+		z *= Math.sin(yangle);
+		// rotation along z
+		y *= Math.sin(zangle);
+		x *= Math.cos(zangle);
+
+		return new double[] { x, y, z };
+
+	}
+
+	/**
+	 * 
+	 * @param i
+	 *            Arm to check
+	 * @return
+	 */
+	private boolean intersects(int i) {
+		
+		return false;
 	}
 }
